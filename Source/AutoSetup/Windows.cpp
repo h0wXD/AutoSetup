a0d1c34427201752dde6b8fa62bef55ac6eda4c6
h0wXD <h0wXD@users.noreply.github.com>
2015-04-25 09:36:44 +0200

    Switched from Microsoft Research Detours to MinHook.

--- Source/AutoSetup/Windows.cpp
+++ Source/AutoSetup/Windows.cpp
@@ -5,7 +5,22 @@
 
 std::unique_ptr<HookManager> g_pHookManager;
 
-HWND WINAPI Hook_CreateWindowEx(DWORD dwExStyle, asLPCSTR lpClassName, asLPCSTR lpWindowName, DWORD dwStyle, int X, int Y, int nWidth, int nHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance, LPVOID lpParam)
+HWND WINAPI Hook_CreateWindowExA(DWORD dwExStyle, LPCSTR lpClassName, LPCSTR lpWindowName, DWORD dwStyle, int X, int Y, int nWidth, int nHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance, LPVOID lpParam)
 {
-	return g_pHookManager->CreateWindowEx(dwExStyle, lpClassName, lpWindowName, dwStyle, X, Y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
+	return g_pHookManager->CreateWindowExA(dwExStyle, lpClassName, lpWindowName, dwStyle, X, Y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
+}
+
+HWND WINAPI Hook_CreateWindowExW(DWORD dwExStyle, LPCWSTR lpClassName, LPCWSTR lpWindowName, DWORD dwStyle, int X, int Y, int nWidth, int nHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance, LPVOID lpParam)
+{
+	return g_pHookManager->CreateWindowExW(dwExStyle, lpClassName, lpWindowName, dwStyle, X, Y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
+}
+
+BOOL WINAPI Hook_GetMessageA(LPMSG lpMsg, HWND hWnd, UINT wMsgFilterMin, UINT wMsgFilterMax)
+{
+	return g_pHookManager->GetMessageA(lpMsg, hWnd, wMsgFilterMin, wMsgFilterMax);
+}
+
+BOOL WINAPI Hook_GetMessageW(LPMSG lpMsg, HWND hWnd, UINT wMsgFilterMin, UINT wMsgFilterMax)
+{
+	return g_pHookManager->GetMessageW(lpMsg, hWnd, wMsgFilterMin, wMsgFilterMax);
 }
